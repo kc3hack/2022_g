88aac40c3e099275a99f97a839a411fb6dae3fd3
s256015 <89733374+Ishibashi-cgp@users.noreply.github.com>
2022-03-20 20:00:19 +0900

    今存在するスクリプトへの、コメント付け足し

--- Unity/kc3Hack_2022_G/Assets/Scripts/InGame/Item.cs
+++ Unity/kc3Hack_2022_G/Assets/Scripts/InGame/Item.cs
@@ -2,37 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// アイテムの特徴用スクリプト
 public class Item : MonoBehaviour
 {
-    Rigidbody2D rb;
-    bool moved;
 
+    Rigidbody2D rb; // Rigidbody2Dコンポーネントをスクリプトで操作する為の変数
+    bool moved;     // 「既に操作されたかどうか」
+    int itemScore;  // （各種アイテムが持つ、固有のスコア　の予定）
+
+
+    // 最初に実行される
     private void Start()
     {
+        // このスクリプトがアタッチされているGameObjectの、Rigidbody2Dコンポーネントを代入する
         rb = GetComponent<Rigidbody2D>();
+
+        // Rigidbody2D の bodyType を Kinematic に変更する
         rb.bodyType = RigidbodyType2D.Kinematic;
+
+        // 「最初はまだ操作されていない」
         moved = false;
     }
 
+
+    // マウス（もしくは指）でドラッグされている間、毎フレーム処理
     void OnMouseDrag()
     {
+        // もし操作済みでないならば
         if (moved == false)
         {
+            // 画面上のマウスの座標を取り出す
             Vector2 pointScreen = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
+            // マウスの座標を、ゲーム内の座標に変換する
             Vector2 pointWorld = Camera.main.ScreenToWorldPoint(pointScreen);
 
+            // 変換した座標の位置に、アイテムを動かす
             this.transform.position = pointWorld;
         }
     }
 
 
+    // もしドラッグされ終わったら
     private void OnMouseUp()
     {
+        // もし操作済みでないならば
         if (moved == false)
         {
+            // Rigidbody2D の bodyType を Dynamic に変更する
             rb.bodyType = RigidbodyType2D.Dynamic;
+
+            // 「操作済み」
             moved = true;
         }
     }
+
 }
--- Unity/kc3Hack_2022_G/Assets/Scripts/SceneChanger.cs
+++ Unity/kc3Hack_2022_G/Assets/Scripts/SceneChanger.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// シーン遷移処理用スクリプト
 public class SceneChanger : MonoBehaviour
 {
-
+    // まず最初に実行
     private void Start()
     {
+        // このスクリプトがアタッチされたGameObjectが、シーン遷移時に破棄されなくなる
         DontDestroyOnLoad(this);
     }
+
 
     // シーン変更用のメソッド  sceneNameで遷移先Scene指定
     public void ChangeScene(string sceneName)
@@ -17,18 +20,23 @@ public class SceneChanger : MonoBehaviour
         switch (sceneName)
         {
             case "GameScene":
+                // 1.0f秒後、メソッド"ChangeToGameScene"を実行
                 Invoke("ChangeToGameScene", 1.0f);
                 break;
             default:
+                // ログに"SceneNameError"と表示
                 Debug.Log("SceneNameError");
                 break;
         }
 
     }
 
+
     // GameSceneに遷移する用のメソッド
     public void ChangeToGameScene()
     {
+        // シーン"GameScene"に遷移
         SceneManager.LoadScene("GameScene");
     }
+
 }
