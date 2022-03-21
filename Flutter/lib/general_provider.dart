2a8cde8f8cb788d51e6c3ea9c6c0b2eb7dc5714b
井手翔陽 <shouhi605@gmail.com>
2022-03-22 01:54:18 +0900

    add review model

---
+++ Flutter/lib/general_provider.dart
@@ -0,0 +1,11 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// // firebaseAuthのインスタンスを提供
+// final firebaseAuthProvider =
+//     Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+
+// // firebaseStoreのインスタンスの提供
+// final firebaseFirestoreProvider =
+//     Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
