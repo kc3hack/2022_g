97554e3f565cf00d92c45d6cdbc5863d3b4bd0b4
s256015 <89733374+Ishibashi-cgp@users.noreply.github.com>
2022-03-24 20:03:01 +0900

    前回のコミットで抜けていたと思われるファイルをコミット（お騒がせしてすみませんでした）

---
+++ Flutter/unity/kc3Hack_2022_G/Assets/Scripts/InGame/CantOperateTriggrt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CantOperateTriggrt : MonoBehaviour
+{
+    [SerializeField] InGameManager igm;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (igm.currentItemGO != null)
+        {
+            if (collision.gameObject == igm.currentItemGO)
+            {
+                if (igm.currentItem != null)
+                {
+                    igm.currentItem.finishOperate();
+                }
+            }
+        }
+    }
+
+}
