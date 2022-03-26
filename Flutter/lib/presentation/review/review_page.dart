80301fba69e0d375f97f8c349a02462fa6d9e4ea
井手翔陽 <shouhi605@gmail.com>
2022-03-27 02:52:15 +0900

    review page 更新

--- Flutter/lib/presentation/review/review_page.dart
+++ Flutter/lib/presentation/review/review_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:team_g/constants/constants.dart';
 import 'package:team_g/presentation/review/add_review_page.dart';
 
@@ -101,84 +100,4 @@ class PostPage extends ConsumerWidget {
       ),
     );
   }
-}
-
-class MapView extends HookWidget {
-  final Completer<GoogleMapController> _mapController = Completer();
-  // 初期表示位置を渋谷駅に設定
-  // final Position _initialPosition = Position(
-  //   latitude: 35.658034,
-  //   longitude: 139.701636,
-  //   timestamp: DateTime.now(),
-  //   altitude: 0,
-  //   accuracy: 0,
-  //   heading: 0,
-  //   floor: null,
-  //   speed: 0,
-  //   speedAccuracy: 0,
-  // );
-
-  @override
-  Widget build(BuildContext context) {
-    // 初期表示座標のMarkerを設定
-    final initialMarkers = {
-      // _initialPosition.timestamp.toString(): Marker(
-      //   markerId: MarkerId(_initialPosition.timestamp.toString()),
-      //   position: LatLng(_initialPosition.latitude, _initialPosition.longitude),
-      // ),
-    };
-    // final position = useState<Position>(_initialPosition);
-    // final markers = useState<Map<String, Marker>>(initialMarkers);
-
-    // _setCurrentLocation(position, markers);
-    // _animateCamera(position);
-
-    return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        myLocationButtonEnabled: false,
-        // 初期表示位置は渋谷駅に設定
-        initialCameraPosition: CameraPosition(
-          target: LatLng(0.0, 1.0),
-          zoom: 14.4746,
-        ),
-        onMapCreated: _mapController.complete,
-        // markers: markers.value.values.toSet(),
-      ),
-    );
-  }
-
-  // Future<void> _setCurrentLocation(ValueNotifier<Position> position,
-  //     ValueNotifier<Map<String, Marker>> markers) async {
-  //   final currentPosition = await Geolocator.getCurrentPosition(
-  //     desiredAccuracy: LocationAccuracy.high,
-  //   );
-
-  //   const decimalPoint = 3;
-  //   // 過去の座標と最新の座標の小数点第三位で切り捨てた値を判定
-  //   if ((position.value.latitude).toStringAsFixed(decimalPoint) !=
-  //           (currentPosition.latitude).toStringAsFixed(decimalPoint) &&
-  //       (position.value.longitude).toStringAsFixed(decimalPoint) !=
-  //           (currentPosition.longitude).toStringAsFixed(decimalPoint)) {
-  //     // 現在地座標にMarkerを立てる
-  //     final marker = Marker(
-  //       markerId: MarkerId(currentPosition.timestamp.toString()),
-  //       position: LatLng(currentPosition.latitude, currentPosition.longitude),
-  //     );
-  //     markers.value.clear();
-  //     markers.value[currentPosition.timestamp.toString()] = marker;
-  //     // 現在地座標のstateを更新する
-  //     position.value = currentPosition;
-  //   }
-  // }
-
-  // Future<void> _animateCamera(ValueNotifier<Position> position) async {
-  //   final mapController = await _mapController.future;
-  //   // 現在地座標が取得できたらカメラを現在地に移動する
-  //   await mapController.animateCamera(
-  //     CameraUpdate.newLatLng(
-  //       LatLng(position.value.latitude, position.value.longitude),
-  //     ),
-  //   );
-  // }
 }
