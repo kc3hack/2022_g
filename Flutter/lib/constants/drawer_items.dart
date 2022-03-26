6474f6afd673a69a8256e4526a23a2e5cfb64d78
井手翔陽 <shouhi605@gmail.com>
2022-03-26 23:19:42 +0900

    constants追加

---
+++ Flutter/lib/constants/drawer_items.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:protain/Models/DrawerItem.dart';
+
+// class DrawerItems {
+//   static const home = DrawerItem(title: 'ホーム', icon: FontAwesomeIcons.home);
+//   static const review = DrawerItem(title: 'レビュー', icon: FontAwesomeIcons.heart);
+//   static const contact = DrawerItem(title: 'お問い合わせ', icon: Icons.mail);
+//   static const setting = DrawerItem(title: '設定', icon: Icons.settings);
+//   static const logout = DrawerItem(title: 'ログアウト', icon: Icons.logout);
+
+//   static final List<DrawerItem> all = [
+//     home,
+//     review,
+//     contact,
+//     setting,
+//     logout,
+//   ];
+// }
