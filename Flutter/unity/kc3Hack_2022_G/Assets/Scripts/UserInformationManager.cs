b427563d41baad0c9e135267c98f10c35dc59765
s256015 <89733374+Ishibashi-cgp@users.noreply.github.com>
2022-03-27 12:36:02 +0900

    微調整

--- Flutter/unity/kc3Hack_2022_G/Assets/Scripts/UserInformationManager.cs
+++ Flutter/unity/kc3Hack_2022_G/Assets/Scripts/UserInformationManager.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class UserInformationManager : MonoBehaviour
 {
-    // 商品の購入回数を、カテゴリ別に管理する用の辞書
-    public Dictionary<string, int> itemCounts; 
-    
+    public Dictionary<string, int> itemCounts;
+
+    string[] cats = { "onigiri", "shokupan", "soda", "chicken", "bottle", "snack" };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +17,10 @@ public class UserInformationManager : MonoBehaviour
 
     public void loadItemCounts()
     {
-        // 一旦辞書内を初期化する
         itemCounts.Clear();
 
-        // ここで購入回数を読み込み、辞書に記入する。
-        for (int i = 0; i < 0; i++) {
-            Debug.Log("^ ~~ ^");
+        foreach (string cat in cats) {
+            itemCounts.Add(cat, 1);
         }
-        // テスト用
-        itemCounts.Add("onigiri", 1);
-        itemCounts.Add("shokupan", 1);
-        itemCounts.Add("soda", 1);
-        itemCounts.Add("chicken", 1);
-        itemCounts.Add("bottle", 1);
-        itemCounts.Add("snack", 1);
-
-        Debug.Log("Loading Complete.");
     }
 }
